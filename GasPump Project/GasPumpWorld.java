98010d18a3c59705201ab699270f74e9d688244e
VimanyuAgg <vimanyu1022007@gmail.com>
2017-03-25 14:34:03 -0700

    created gaspumpWorld prepare() method

--- GasPump Project/GasPumpWorld.java
+++ GasPump Project/GasPumpWorld.java
@@ -17,5 +17,25 @@ public class GasPumpWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    
+    /**
+     * Prepare the world for the start of the program. That is: create the initial
+     * objects and add them to the world.
+     */
+    private void prepare(){
+        GasPumpMachine gasMachine = new GasPumpMachine();
+        addObject(gasMachine, 0, 0);
+        addObject( new RealCard(), 57, 71 );
+        addObject( new FakeCard(), 57, 100 );
+        addObject( new DisplayScreen(), 367, 367 ) ;
+        addObject( new Keypad(), 367, 550);
+        addObject( new NozzleHole(), 600, 450);
+        addObject( new Nozzle(), 600,450);
+        
+      
+       
     }
 }
