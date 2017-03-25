9aa8dd6c8ab36cb161e8823c29da3f237bd97830
gurnoors <gurnoor@gmail.com>
2017-03-25 14:26:30 -0700

    Added some actors

---
+++ GasPump Project/CancelTransaction.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CancelTransaction here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CancelTransaction extends ScreenButtons
+{
+    /**
+     * Act - do whatever the CancelTransaction wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
