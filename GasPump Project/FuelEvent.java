fafc0966602c54cd73b0a58339602ea8102c3885
Sparsh <sidanasparsh@gmail.com>
2017-05-10 19:54:50 -0700

    Made keypad keys working and displayed fuel prize

--- GasPump Project/FuelEvent.java
+++ GasPump Project/FuelEvent.java
@@ -12,19 +12,29 @@ public class FuelEvent implements Component
     
     if(gpState.getState() == State.isValidatedButUnfueled){
         //do not proceed until proper state is set
+        Fuel93 f93 = GasPumpWorld.gpWorld.getObjects(Fuel93.class).get(0);
+        Fuel87 f87 = GasPumpWorld.gpWorld.getObjects(Fuel87.class).get(0);
+        Fuel89 f89 = GasPumpWorld.gpWorld.getObjects(Fuel89.class).get(0);
         System.out.println("Fuel event Start Triggered");
         GasPumpWorld.gpWorld.removeObjects(GasPumpWorld.gpWorld.getObjects(DisplayScreen.class));
         DisplayScreen ds = new DisplayScreen("Please select the Fuel Type");
+       // DisplayScreen ds2 = new DisplayScreen(f93.getPrise());
+        //DisplayScreen ds3 = new DisplayScreen(f89.getPrise());
+        //DisplayScreen ds4 = new DisplayScreen(f87.getPrise());
+         //GasPumpWorld.gpWorld.addObject(ds2,351,259);
+         //GasPumpWorld.gpWorld.addObject(ds3,421,259);
+         //GasPumpWorld.gpWorld.addObject(ds4,491,259);
+         GasPumpWorld.gpWorld.addObject(ds,320,180); 
            
-           GasPumpWorld.gpWorld.addObject(ds,350,250);
-    Fuel93 f93 = GasPumpWorld.gpWorld.getObjects(Fuel93.class).get(0);
+          // GasPumpWorld.gpWorld.addObject(ds,350,250);
+
     f93.setToWorkingState();
     //System.out.println("Commanded Fuel93 to start working");
-    Fuel87 f87 = GasPumpWorld.gpWorld.getObjects(Fuel87.class).get(0);
+
     f87.setToWorkingState();
     
 
-    Fuel89 f89 = GasPumpWorld.gpWorld.getObjects(Fuel89.class).get(0);
+
     f89.setToWorkingState();
     }
     
